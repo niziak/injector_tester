1cc1c2a5fbba003394aa5a4343e5d3c410b983da
Niziak <niziak@spox.org>
2013-09-27 14:46:39 +0200

    I2CMaster library adaptation to project

--- lib/i2cmaster/twimaster.c
+++ lib/i2cmaster/twimaster.c
@@ -14,12 +14,19 @@
 
 /* define CPU frequency in Mhz here if not defined in Makefile */
 #ifndef F_CPU
+#error "F_CPU!"
 #define F_CPU 4000000UL
 #endif
 
 /* I2C clock in Hz */
-#define SCL_CLOCK  100000L
+//#define SCL_CLOCK  100000L
+#define SCL_CLOCK  30000L
 
+/*
+ * 10us period =100kHz
+ * 7us period
+ * 62,5us (TWSR=1 TWBR=255 16mhz) = 16kHz
+ */
 
 /*************************************************************************
  Initialization of the I2C bus interface. Need to be called only once
@@ -27,9 +34,17 @@
 void i2c_init(void)
 {
   /* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
-  
-  TWSR = 0;                         /* no prescaler */
-  TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
+  /* TWSR    PRESCALER
+   *  0        /1
+   *  1        /4
+   *  2        /16
+   *  3        /64
+   */
+  #define TWI_PRESCALER   4
+  TWSR = 1;
+  TWBR = ((F_CPU/SCL_CLOCK)/(TWI_PRESCALER)-16)/2;  /* must be > 10 for stable operation */
+  //TWSR = 1;
+  //TWBR=255;
 
 }/* i2c_init */
 
