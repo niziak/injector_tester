a4ef9e244f40f4266e4a0757eed386a7d519eb10
Niziak <niziak@spox.org>
2013-09-30 10:13:50 +0200

    GCC  __attribute__ ((format (printf, 1, 2))) for printf like functions

--- lib/hal_lcd.c
+++ lib/hal_lcd.c
@@ -10,7 +10,24 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void LCD_vPrintf(const char *format, ...) __attribute__ ((format (printf, 1, 2)))
+#ifdef __GNUC__
+/*
+ *  format (archetype, string-index, first-to-check)
+ *
+ * The parameter archetype determines how the format string is interpreted, and should be printf, scanf, strftime or strfmon.
+ * (You can also use __printf__, __scanf__, __strftime__ or __strfmon__.)
+ * The parameter string-index specifies which argument is the format string argument (starting from 1),
+ * while first-to-check is the number of the first argument to check against the format string.
+ * For functions where the arguments are not available to be checked (such as vprintf),
+ * specify the third parameter as zero. In this case the compiler only checks the format string for consistency.
+ * For strftime formats, the third parameter is required to be zero.
+ * Since non-static C++ methods have an implicit this argument,
+ * the arguments of such methods should be counted from two, not one, when giving values for string-index and first-to-check.
+ */
+
+void LCD_vPrintf(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+#endif
+void LCD_vPrintf(const char *format, ...)
 {
 	char acLineBuf[LCD_COLS+1];
 
@@ -20,8 +37,10 @@ void LCD_vPrintf(const char *format, ...) __attribute__ ((format (printf, 1, 2))
 	va_end (args);
 	LCD_vPuts (&acLineBuf[0]);
 }
-
-void LCD_vPrintf_P(const char *format, ...) __attribute__ ((format (printf, 1, 2)))
+#ifdef __GNUC__
+void LCD_vPrintf_P(const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+#endif
+void LCD_vPrintf_P(const char *format, ...)
 {
     char acLineBuf[LCD_COLS+1];
 
@@ -31,3 +50,5 @@ void LCD_vPrintf_P(const char *format, ...) __attribute__ ((format (printf, 1, 2
     va_end (args);
     LCD_vPuts (&acLineBuf[0]);
 }
+
+
--- log.c
+++ log.c
@@ -11,11 +11,28 @@
 #include <config.h>
 #include <lib/hal_lcd.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include <avr/pgmspace.h>
 #include <tools.h>
 
-extern void LOG_Log_P   (const char *format, ...) __attribute__ ((format (printf, 1, 2)))
+#ifdef __GNUC__
+/*
+ *  format (archetype, string-index, first-to-check)
+ *
+ * The parameter archetype determines how the format string is interpreted, and should be printf, scanf, strftime or strfmon.
+ * (You can also use __printf__, __scanf__, __strftime__ or __strfmon__.)
+ * The parameter string-index specifies which argument is the format string argument (starting from 1),
+ * while first-to-check is the number of the first argument to check against the format string.
+ * For functions where the arguments are not available to be checked (such as vprintf),
+ * specify the third parameter as zero. In this case the compiler only checks the format string for consistency.
+ * For strftime formats, the third parameter is required to be zero.
+ * Since non-static C++ methods have an implicit this argument,
+ * the arguments of such methods should be counted from two, not one, when giving values for string-index and first-to-check.
+ */
+extern void LOG_Log_P   (const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+#endif
+extern void LOG_Log_P   (const char *format, ...)
 {
     va_list args;
     va_start (args, format);
@@ -36,7 +53,10 @@ extern void LOG_Log_P   (const char *format, ...) __attribute__ ((format (printf
 //  LCD_vClrScr();
 }
 
-extern void LOG_Log   (const char *format, ...) __attribute__ ((format (printf, 1, 2)))
+#ifdef __GNUC__
+extern void LOG_Log   (const char *format, ...) __attribute__ ((format (printf, 1, 2)));
+#endif
+extern void LOG_Log   (const char *format, ...)
 {
     va_list args;
     va_start (args, format);
@@ -57,7 +77,10 @@ extern void LOG_Log   (const char *format, ...) __attribute__ ((format (printf, 
 //	LCD_vClrScr();
 }
 
-void LOG_Reset (char * message) __attribute__ ((format (printf, 1, 2)))
+#ifdef __GNUC__
+void LOG_Reset (char * message) __attribute__ ((format (printf, 1, 0)));
+#endif
+void LOG_Reset (char * message)
 {
     printf_P (PSTR("RESET:\n"));
     puts (message);
@@ -67,9 +90,11 @@ void LOG_Reset (char * message) __attribute__ ((format (printf, 1, 2)))
 	LCD_vPuts_P(PSTR("RESET:"));
 	LCD_vGotoXY(0,1);
 	LCD_vPuts (message);
+	cli(); // TODO
 	for (;;)
 	{
 	    LCD_BL_ALTER;
 	    _delay_ms(500);
 	}
 }
+
