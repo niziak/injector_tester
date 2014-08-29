dc56962a807e359009d03283e1310fc412b9bd50
Niziak <niziak@spox.org>
2014-08-29 10:45:38 +0200

    Excel document to calulate timers

--- 02_serial.bat
+++ 02_serial.bat
@@ -1,4 +1,8 @@
 @echo off
-REM tools_win32\putty.exe -load "COM25 115"
-START tools_win32\putty.exe -serial com25 -sercfg 115200,8,n,1,N
+IF "nizinski_w" == "%USER%" (
+	START tools_win32\putty.exe -load "COM25 115"
+) else (
+	START tools_win32\putty.exe -serial com25 -sercfg 115200,8,n,1,N
+)
+
 
